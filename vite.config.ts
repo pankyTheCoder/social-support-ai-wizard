a074b5d7430152d0d2ce0a84765300065f67a9a3
pankyTheCoder <pankaj.humnabadkar@gmail.com>
2025-05-29 10:26:17 +0530

    feat: performance and accessibility issue fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import compression from 'vite-plugin-compression';
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -8,8 +9,26 @@ export default defineConfig(() => ({
     host: "::",
     port: 8080,
   },
+  build : {
+    minify: true, // enable minification
+    target: "esbuild", // target modern browsers
+    outDir: "dist", // output directory
+    sourcemap: true, // generate source maps for debugging
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      }
+    }
+  },
   plugins: [
-    react()
+    react(),
+    compression({
+      algorithm: 'gzip',
+      ext: '.gz', // optional: .gz extension
+      threshold: 1024, // only assets bigger than 1KB are compressed
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
